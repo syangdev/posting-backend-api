3e6b95611a5962682a41694503c325228c565a1e
Sai Yang <sai.yang@ymail.com>
2019-03-14 00:02:57 -0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private logger: Logger) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/test")
+  test(): string {
+    const testString = 'testing ... '+ Date.now();
+    this.logger.log(testString);
+    return testString;
   }
 }
