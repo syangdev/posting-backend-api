a78a17d23a0b328cfd4bfc8138d50fc4221a2649
Sai Yang <sai.yang@ymail.com>
2019-03-18 00:28:16 -0700

    Return simple messages on posts requests

--- src/controllers/posts/posts.controller.ts
+++ src/controllers/posts/posts.controller.ts
@@ -7,27 +7,35 @@ export class PostsController {
 
     @Get()
     public getPosts() {
-        this.logger.log(`[${Date.now()}] [GET] ${RouteRoot}/posts`);
+        const message: string = `[${Date.now()}] [GET] ${RouteRoot}/posts`;
+        this.logger.log(message);
+        return {message};
     }
 
     @Get(":id")
     public getPostById(
         @Param("id") postId: string
     ) {
-        this.logger.log(`[${Date.now()}] [GET] ${RouteRoot}/posts/${postId}`);
+        const message: string = `[${Date.now()}] [GET] ${RouteRoot}/posts/${postId}`;
+        this.logger.log(message);
+        return {message};
     }
 
     @Post("")
     public createPost(
         @Param("id") postId: string,
     ) {
-        this.logger.log(`[${Date.now()}] [POST] ${RouteRoot}/posts`);
+        const message: string = `[${Date.now()}] [POST] ${RouteRoot}/posts`;
+        this.logger.log(message);
+        return {message};
     }
 
     @Put(":id")
     public updatePost(
         @Param("id") postId: string
     ) {
-        this.logger.log(`[${Date.now()}] [PUT] ${RouteRoot}/posts/${postId}`);
+        const message: string = `[${Date.now()}] [PUT] ${RouteRoot}/posts/${postId}`;
+        this.logger.log(message);
+        return {message};
     }
 }
